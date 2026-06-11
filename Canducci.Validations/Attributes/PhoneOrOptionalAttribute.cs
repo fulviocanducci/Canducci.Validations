340bc37368d67b5976db20ee490d2f930e35b055
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2026-06-11 15:43:38 -0300

    update phone validations

---
+++ Canducci.Validations/Attributes/PhoneOrOptionalAttribute.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Canducci.Validations.Attributes
+{
+    public class PhoneOrOptionalAttribute : ValidationAttribute, IClientModelValidator
+    {
+        public string Formats { get; }
+
+        public PhoneOrOptionalAttribute(string formats = "10,11")
+        {
+            Formats = formats;
+            ErrorMessage = "Telefone inválido.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            string text = value.ToString();
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return ValidationResult.Success;
+            }
+            string numbers = new string(text.Where(char.IsDigit).ToArray());
+            IEnumerable<int> formatsArray = Formats.Split(',').Select(x => int.Parse(x.Trim()));
+            if (formatsArray.Contains(numbers.Length))
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult(ErrorMessage);
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-phone-or-optional", ErrorMessage);
+            MergeAttribute(context.Attributes, "data-val-phone-or-optional-formats", Formats);
+        }
+
+        private static bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+            attributes.Add(key, value);
+            return true;
+        }
+    }
+}
---
+++ Canducci.Validations/Scripts/jquery.validate.phoneoroptional.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    $.validator.addMethod("phone-or-optional", function (value, element, formats) {
+        if (!value) {
+            return true;
+        }
+        const text = value.replace(/\D/g, "");
+        const formatsArray = formats ? formats.split(",") : ["10", "11"];
+        return formatsArray.some((x) => text.length === parseInt(x, 10));
+    });
+    $.validator.unobtrusive.adapters.addSingleVal("phone-or-optional", "formats");
+})(jQuery);
