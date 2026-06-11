4e7987e7f8c1d64cfada2a8272519460020e7026
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2026-06-11 11:08:21 -0300

    up

---
+++ Canducci.Validations/Attributes/MoneyOrOptionalAttribute.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Canducci.Validations.Attributes
+{
+    public class MoneyOrOptionalAttribute : ValidationAttribute, IClientModelValidator
+    {
+        public string DecimalSeparator { get; }
+        public string ThousandSeparator { get; }
+
+        public MoneyOrOptionalAttribute(string decimalSeparator = ".", string thousandSeparator = ",")
+        {
+            DecimalSeparator = decimalSeparator;
+            ThousandSeparator = thousandSeparator;
+            ErrorMessage = "Value money inválid.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+            if (value is decimal)
+            {
+                return ValidationResult.Success;
+            }
+            if (value is float)
+            {
+                return ValidationResult.Success;
+            }
+            if (value is double)
+            {
+                return ValidationResult.Success;
+            }
+            string text = value.ToString().Trim();
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return ValidationResult.Success;
+            }
+            text = text.Replace(ThousandSeparator, "").Replace(DecimalSeparator, ".");
+            return decimal
+                .TryParse(text, System.Globalization.NumberStyles.Number, System.Globalization.CultureInfo.InvariantCulture, out _)
+                    ? ValidationResult.Success
+                    : new ValidationResult(ErrorMessage);
+        }
+
+        public void AddValidation(ClientModelValidationContext context)
+        {
+            MergeAttribute(context.Attributes, "data-val", "true");
+            MergeAttribute(context.Attributes, "data-val-money", ErrorMessage);
+            MergeAttribute(context.Attributes, "data-val-money-decimal", DecimalSeparator);
+            MergeAttribute(context.Attributes, "data-val-money-thousand", ThousandSeparator);
+        }
+
+        private bool MergeAttribute(IDictionary<string, string> attributes, string key, string value)
+        {
+            if (attributes.ContainsKey(key))
+            {
+                return false;
+            }
+            attributes.Add(key, value);
+            return true;
+        }
+    }
+}
---
+++ Canducci.Validations/Scripts/jquery.validate.moneyoroptional.js
@@ -0,0 +1,18 @@
+﻿(function ($) {
+    $.validator.addMethod("money", function (value, element, params) {
+        if (!value) return true;
+        var decimalSeparator = params.decimal || ",";
+        var thousandSeparator = params.thousand || ".";
+        value = value.split(thousandSeparator).join("");
+        value = value.replace(decimalSeparator, ".");
+        return !isNaN(parseFloat(value));
+    });
+
+    $.validator.unobtrusive.adapters.add("money", ["decimal", "thousand"], function (options) {
+        options.rules["money"] = {
+            decimal: options.params.decimal,
+            thousand: options.params.thousand,
+        };
+        options.messages["money"] = options.message;
+    });
+})(jQuery);
